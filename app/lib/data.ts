8ff435343a2d0db6f7c165439084c417aa2e7999
elisha1995 <elishasamuel01@gmail.com>
2024-02-20 10:17:15 +0000

    Handling errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-   // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -173,6 +173,8 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+
+    console.log(invoice); // Invoice is an empty array []
 
     return invoice[0];
   } catch (error) {
